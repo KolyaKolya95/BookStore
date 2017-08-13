e113e0205e1a14d0b3936e83e111309bcf855e73
KolyaKolya95 <kolya.kolya1495@gmail.com>
2017-08-13 14:35:18 +0300

    initial commit

---
+++ BookStore/WebUI/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class BooksController : Controller
+    {
+        private IBookRepository repository;
+        public int pageSize = 4;
+
+        public BooksController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            BookListViewModel model = new BookListViewModel
+            {
+                Books = repository.Books
+                .OrderBy(book => book.BookId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotatlItems = repository.Books.Count()
+                }
+            };
+
+            return View(model);
+        }
+    }
+}
